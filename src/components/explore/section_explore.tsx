4184f89fca7eebb439488be416d10257040b4f57
kuetabby <vansmuhammad@gmail.com>
2021-01-18 14:15:47 +0700

    update applying fetch with gql

--- src/components/explore/section_explore.tsx
+++ src/components/explore/section_explore.tsx
@@ -1,52 +1,19 @@
 import * as React from "react";
 import Image from "next/image";
 import Link from "next/link";
+interface Props {
+  data: {
+    id: string;
+    name: string;
+    profile_img: string | null;
+    cover_img: string | null;
+    description: string | null;
+    category: string;
+  }[];
+}
 
-interface Props {}
-
-export const SectionExplore: React.FC<Props> = () => {
-  const renderSection = () => (
-    <Link href="/">
-      <div className="w-screen lg:w-1/4 shadow-lg my-4 md:mt-0 cursor-pointer group">
-        <Image
-          height={100}
-          width={200}
-          objectFit="cover"
-          quality={100}
-          layout="responsive"
-          className="bg-center bg-no-repeat"
-          src="https://imgsrv.voi.id/-Rs68hmfCJCfwrs8m-HzEzeatUa3kRNAIYffkzcTDLI/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8zNjIzN2QyNy1kZWQ1LTRjMzMtYTM4Zi02Y2M1ZWNlNmFiYWMvMjAyMDA1MTAwNzQyLW1haW4uY3JvcHBlZF8xNTg5MDcxMzk2LmpwZw.jpg"
-        />
-        <div className="flex flex-row items-center h-12 p-auto mb-12 mx-4">
-          <div className="flex flex-row items-center justify-center h-24">
-            <Image
-              height={100}
-              width={100}
-              quality={100}
-              className="rounded-full"
-              src="https://imgsrv.voi.id/-Rs68hmfCJCfwrs8m-HzEzeatUa3kRNAIYffkzcTDLI/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8zNjIzN2QyNy1kZWQ1LTRjMzMtYTM4Zi02Y2M1ZWNlNmFiYWMvMjAyMDA1MTAwNzQyLW1haW4uY3JvcHBlZF8xNTg5MDcxMzk2LmpwZw.jpg"
-            />
-          </div>
-          <p className="w-1/5 text-left mx-4">Loso</p>
-        </div>
-        <div className="p-auto shadow-lg my-6 mx-4 bg-gray-200 w-4/12 rounded">
-          <p className="p-2 mt-2 mx-0 text-center font-bold">Dancer</p>
-        </div>
-        <div className="h-32 overflow-auto">
-          <p className="p-auto my-6 mx-4 text-blue-600">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-        </div>
-        <button
-          type="button"
-          className="p-2 my-2 md:my-4 mx-auto flex justify-center font-weight-semibold rounded shadow-lg bg-gray-50 text-red-500 group-hover:bg-red-600 group-hover:text-white"
-        >
-          Lihat Halaman
-        </button>
-      </div>
-    </Link>
-  );
+export const SectionExplore: React.FC<Props> = ({ data }) => {
+  const default_img_url = `https://imgsrv.voi.id/-Rs68hmfCJCfwrs8m-HzEzeatUa3kRNAIYffkzcTDLI/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8zNjIzN2QyNy1kZWQ1LTRjMzMtYTM4Zi02Y2M1ZWNlNmFiYWMvMjAyMDA1MTAwNzQyLW1haW4uY3JvcHBlZF8xNTg5MDcxMzk2LmpwZw.jpg`;
 
   return (
     <div className="my-0 mx-auto pt-20">
@@ -55,9 +22,52 @@ export const SectionExplore: React.FC<Props> = () => {
           <h2 className="text-3xl font-semibold">Explore</h2>
         </div>
         <div className="mt-6 mx-auto h-full w-full flex flex-row flex-wrap justify-between">
-          {renderSection()}
-          {renderSection()}
-          {renderSection()}
+          {data.map((item, index) => (
+            <Link href="/" key={index}>
+              <div className="w-screen lg:w-1/4 shadow-lg md:mx-2 my-4 md:mt-0 cursor-pointer group">
+                <Image
+                  height={100}
+                  width={200}
+                  objectFit="cover"
+                  quality={100}
+                  layout="responsive"
+                  className="bg-center bg-no-repeat"
+                  alt="cover photo"
+                  src={item.cover_img || default_img_url}
+                />
+                <div className="flex flex-row items-center h-12 p-auto mb-12 mx-4">
+                  <div className="flex flex-row items-center justify-center h-24">
+                    <Image
+                      height={100}
+                      width={100}
+                      quality={100}
+                      className="rounded-full"
+                      alt="profile photo"
+                      src={item.profile_img || default_img_url}
+                    />
+                  </div>
+                  <p className="text-left mx-4">{item.name}</p>
+                </div>
+                <div className="p-auto shadow-lg my-6 mx-4 bg-gray-200 w-2/5 rounded">
+                  <p className="p-2 mt-2 mx-0 text-center font-bold">
+                    {item.category || "-"}
+                  </p>
+                </div>
+                <div className="h-32 overflow-auto">
+                  <p className="p-auto my-6 mx-4 text-blue-600">
+                    {item.description ||
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
+                  </p>
+                </div>
+                <button
+                  type="button"
+                  className="p-2 my-2 md:my-4 mx-auto w-2/4 flex justify-center font-weight-semibold rounded shadow-lg bg-gray-50 text-red-500 group-hover:bg-red-600 group-hover:text-white"
+                >
+                  Lihat Halaman
+                </button>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,22 +1,67 @@
 import * as React from "react";
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 import { HeaderHomePage, FooterHomePage } from "src/components/homepage";
 import { SectionExplore } from "src/components/explore";
 
-interface Props {}
+interface Props {
+  data: {
+    id: string;
+    name: string;
+    profile_img: string | null;
+    cover_img: string | null;
+    description: string | null;
+    category: string;
+  }[];
+}
 
-const Explore: React.FC<Props> = () => {
+const Explore: React.FC<Props> = ({ data }) => {
   return (
     <div className="w-full h-full">
       <Head>
         <title>Explore</title>
       </Head>
       <HeaderHomePage />
-      <SectionExplore />
+      <SectionExplore data={data} />
       <FooterHomePage />
     </div>
   );
 };
 
 export default Explore;
+
+export const getStaticProps: GetServerSideProps = async () => {
+  const client = new ApolloClient({
+    uri: "https://apresiasi.herokuapp.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        getAllUser {
+          id
+          name
+          profile_img
+          cover_img
+          description
+          category
+        }
+      }
+    `,
+  });
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data: data.getAllUser,
+    },
+  };
+};
