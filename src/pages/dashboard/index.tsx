1f8e342b3766429e2fe58b53b4756665844e09f2
kuetabby <vansmuhammad@gmail.com>
2021-01-19 19:30:26 +0700

    add using authentication to pages

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Router from "next/router";
+
+import { HeaderDashboard, SectionDashboard } from "src/components/dashboard";
+
+import useUser from "src/hooks/useUser";
+
+interface Props {}
+
+const Dashboard: React.FC<Props> = () => {
+  const { loggedOut, mutate } = useUser();
+
+  React.useEffect(() => {
+    mutate();
+    if (loggedOut) {
+      Router.replace("/");
+    }
+  }, [loggedOut]);
+
+  if (loggedOut)
+    return (
+      <div className="w-screen h-screen flex flex-row justify-center items-center">
+        <p className="text-center text-xl text-red-700">Redirecting ...</p>
+      </div>
+    );
+
+  return (
+    <div className="w-full h-full">
+      <HeaderDashboard />
+      <SectionDashboard />
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
+import Router from "next/router";
 
 import { HeaderHomePage, FooterHomePage } from "src/components/homepage";
 import { SectionLogin } from "src/components/login";
 
+import useUser from "src/hooks/useUser";
+
 interface Props {}
 
 const Login: React.FC<Props> = () => {
+  const { user } = useUser();
+
+  React.useEffect(() => {
+    if (user) {
+      Router.replace("/dashboard");
+    }
+  }, [user]);
+
   return (
     <div className="w-full h-full">
       <HeaderHomePage />
