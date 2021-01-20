a2503ed4d61515e9bf58e6399f9e71e5e6a03edb
kuetabby <vansmuhammad@gmail.com>
2021-01-20 18:19:05 +0700

    update cleaning app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,8 @@ import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
 import type { AppProps /*, AppContext */ } from "next/app";
-// import { GraphQLClient } from "graphql-request";
-// import Cookie from "js-cookie";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-// const client = new GraphQLClient(String(process.env.NEXT_PUBLIC_ENDPOINT));
-// const token = Cookie.get("token") || "";
-// // Override all existing headers
-// client.setHeaders({
-//   authorization: token ? token : "",
-// });
 
 export default MyApp;
