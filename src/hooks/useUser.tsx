3f5ef38a5fdffeb9faa85c78d3487c7ccc6b725a
kuetabby <vansmuhammad@gmail.com>
2021-01-20 18:10:37 +0700

    fix authentication

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 import Cookie from "js-cookie";
-import request from "graphql-request";
+import { GraphQLClient } from "graphql-request";
 
 export default function useUser() {
   const { data, mutate, error } = useSWR(
@@ -24,20 +24,26 @@ export default function useUser() {
   return {
     loading,
     loggedOut,
-    user: data,
+    data,
     mutate,
   };
 }
 
-const validationFetcher = async () => {
+const validationFetcher = async (query: string) => {
   // sleep 500
   await new Promise((res) => setTimeout(res, 500));
+
   const token = Cookie.get("token");
+
+  const client = new GraphQLClient(String(process.env.NEXT_PUBLIC_ENDPOINT), {
+    headers: {
+      authorization: token ? token : "",
+    },
+  });
 
   if (token) {
     // authorized
-    return (query: string) =>
-      request(String(process.env.NEXT_PUBLIC_ENDPOINT), query);
+    return client.request(query);
   }
 
   // not authorized
