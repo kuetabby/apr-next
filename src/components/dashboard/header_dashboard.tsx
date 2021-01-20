c149fec54ff4dd9ca88bf02c8f08fb81293e8573
kuetabby <vansmuhammad@gmail.com>
2021-01-20 18:18:22 +0700

    update dashboard

--- src/components/dashboard/header_dashboard.tsx
+++ src/components/dashboard/header_dashboard.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Image from "next/image";
+
+import useUser from "src/hooks/useUser";
 
 interface Props {}
 
+type User = {
+  name: string;
+  profile_img: string | null;
+};
+
 export const HeaderDashboard: React.FC<Props> = () => {
+  const { data, loading } = useUser();
   const router = useRouter();
+
   const pathName = router.pathname
     .split("/")
     .join("")
@@ -13,9 +23,13 @@ export const HeaderDashboard: React.FC<Props> = () => {
     .join("")
     .toLocaleUpperCase();
 
+  const user: User = data && data.getUser;
+
+  const default_img_url = `https://imgsrv.voi.id/-Rs68hmfCJCfwrs8m-HzEzeatUa3kRNAIYffkzcTDLI/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8zNjIzN2QyNy1kZWQ1LTRjMzMtYTM4Zi02Y2M1ZWNlNmFiYWMvMjAyMDA1MTAwNzQyLW1haW4uY3JvcHBlZF8xNTg5MDcxMzk2LmpwZw.jpg`;
+
   return (
-    <header className="w-full fixed z-10 top-0 md:px-8 shadow-lg">
-      <nav className="flex flex-row items-center justify-between p-4 md:p-2">
+    <header className="w-full fixed z-10 top-0 md:px-3 shadow-lg">
+      <nav className="flex flex-row items-center justify-between my-1 p-2 md:p-0">
         <div className="w-2/5 md:w-1/6 flex flex-row justify-between items-center">
           <Link href={"/"}>
             <h1 className="text-2xl font-bold text-red-700 cursor-pointer">
@@ -29,14 +43,28 @@ export const HeaderDashboard: React.FC<Props> = () => {
             View Page
           </button>
         </div>
-        <p className="hidden md:block w-3/5 md:w-2/6 text-center text-lg text-red-700 font-medium md:mx-2">
+        <p className="hidden md:block w-3/5 md:w-4/6 text-center text-lg text-red-700 font-medium md:mx-2">
           {pathName}
         </p>
-        <Link href={"/login"}>
-          <p className="hidden md:block w-2/5 md:w-1/6 text-2xl text-white text-center bg-green-500 font-medium p-1 rounded md:mx-2 my-1 sm:my-0 hover:bg-green-700 cursor-pointer">
-            Login
-          </p>
-        </Link>
+        <div className="hidden md:flex flex-row items-center justify-end w-2/5 md:w-1/6 m-auto">
+          {loading ? (
+            <p>Loading...</p>
+          ) : user ? (
+            <>
+              <Image
+                src={user.profile_img || default_img_url}
+                width={60}
+                height={60}
+                layout="fixed"
+                className="rounded-full w-3/6"
+              />
+              <div className="md:ml-2 w-3/6">
+                <p className="font-bold text-gray-300">{user.name}</p>
+                <p className="font-bold text-gray-300">Creator</p>
+              </div>
+            </>
+          ) : null}
+        </div>
       </nav>
     </header>
   );
--- src/components/dashboard/section_dashboard.tsx
+++ src/components/dashboard/section_dashboard.tsx
@@ -1,12 +1,33 @@
 import * as React from "react";
+import useSWR from "swr";
+
+import { authorizedFetchWithoutVariable } from "src/utils/fetcher";
 
 interface Props {}
 
 export const SectionDashboard: React.FC<Props> = () => {
+  const { data, error } = useSWR(
+    `{
+    getUser {
+        balance
+    }
+}`,
+    authorizedFetchWithoutVariable
+  );
+
+  const isLoading = !data && !error;
+  const balance = data && data.getUser?.balance;
+
   return (
     <div className="my-0 mx-auto pt-20">
       <div className="px-6 md:px-10 py-6">
-        <p className="text-red-700 font-bold text-xl">Rp. 986,218</p>
+        <p className="text-red-700 font-bold text-xl">
+          {isLoading
+            ? "Loading..."
+            : balance
+            ? `Rp. ${Number(balance).toLocaleString()}`
+            : `Rp. 0`}
+        </p>
         <span className="text-blue-500 text-sm">Jumlah Saldo Saat ini</span>
       </div>
     </div>
