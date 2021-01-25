14e749a40a385d2f4317f236422b768d7e3d311f
kuetabby <vansmuhammad@gmail.com>
2021-01-25 15:21:28 +0700

    add service worker offline fallback

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
 import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
+import { useEffect } from "react";
 import type { AppProps /*, AppContext */ } from "next/app";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then((register) => {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              register.scope
+            );
+          })
+          .catch((err) => {
+            console.log("Service Worker registration failed: ", err);
+          });
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
